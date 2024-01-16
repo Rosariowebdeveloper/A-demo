9055d3b76ed3466135268b8c26aa90efb87f72a9
Rosariowebdeveloper <rosariobussines2@gmail.com>
2024-01-16 16:52:58 +0530

    complete a footer and fully site and continue to working animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AboutComponent } from './about/about.component';
 import { ProjectComponent } from './project/project.component';
 import { TestimonalComponent } from './testimonal/testimonal.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ContactComponent } from './contact/contact.component';
     AboutComponent,
     ProjectComponent,
     TestimonalComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
